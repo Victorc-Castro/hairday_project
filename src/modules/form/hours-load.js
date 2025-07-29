564693841c04675c281729e4beca51330c2c0843
Victor <wortsac01@gmail.com>
2025-07-29 19:50:57 -0300

    add selected class

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,12 +1,16 @@
 import dayjs from "dayjs"
+
 import { openingsHours } from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
+
+const hours = document.getElementById("hours")
 
 export function hoursLoad({ date }) {
   const opening = openingsHours.map((hour) => {
     // Recupera somente a hora.
     const [ scheduleHour ] = hour.split(":")
 
-    // Adiciona a hora na data e verificar se está no passado
+    // Adiciona a hora na data e verificar se está no passado.
     const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
 
     return {
@@ -15,5 +19,35 @@ export function hoursLoad({ date }) {
     }
   })
 
-  console.log(opening)
+  // Renderiza os horários.
+  opening.forEach(({hour, available}) => {
+    const li = document.createElement("li")
+
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+    li.textContent = hour
+
+    if(hour === "9:00") {
+      hourHeaderAdd("Manhã")
+    } else if (hour === "13:00") {
+      hourHeaderAdd("Tarde")
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite")
+    }
+
+    hours.append(li)
+  })
+
+  // Adiciona um evento de clique nos horários disponíveis.
+  hoursClick()
+
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+  header.classList.add("hour-period")
+  header.textContent = title
+
+  hours.append(header)
 }
