79ce345641dba28e822b463e05f8a987c07aec46
Victor <wortsac01@gmail.com>
2025-07-29 22:02:23 -0300

    add schedules

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,12 +1,20 @@
 import { hoursLoad } from "../form/hours-load.js"
+import { schedulesShow } from "../schedules/show.js"
+import { scheduleFetchByDay } from '../../services/schedule-fetch-by-day.js'
 
 // Seleciona o input de data.
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // Obtém a data do input.
   const date = selectedDate.value
 
+  // Busca na API os agendamentos.
+  const dailySchedules = await scheduleFetchByDay({ date })
+
+  // Exibe os agendamentos.
+  schedulesShow({ dailySchedules })
+
   // Redenriza as horas disponíveis.
-  hoursLoad({ date })
+  hoursLoad({ date, dailySchedules })
 }
